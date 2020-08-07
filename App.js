8f4d36edf6953e23d78bbb72144b0a31cd3eaa61
Anatolii <pranzpro@gmail.com>
2020-08-07 20:18:14 +0300

    add Redux

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react'
+import { Provider } from 'react-redux'
 import { StyleSheet, Text, View } from 'react-native'
 import { AppLoading } from 'expo'
 import { AppNavigation } from './src/navigation/AppNavigation'
 import { bootstrap } from './src/bootstrap'
+import store from './src/redux/store'
 
 export default function App() {
-  const [isReady, setIsReady] = useState(false)
+	const [isReady, setIsReady] = useState(false)
 
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={bootstrap}
-        onFinish={() => setIsReady(true)}
-        onError={err => console.log(err)}
-      />
-    )
-  }
+	if (!isReady) {
+		return (
+			<AppLoading
+				startAsync={bootstrap}
+				onFinish={() => setIsReady(true)}
+				onError={err => console.log(err)}
+			/>
+		)
+	}
 
-  return <AppNavigation />
+	return (
+		<Provider store={store}>
+			<AppNavigation />
+		</Provider>
+	)
 }
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,13 +1,55 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import {
+	View,
+	Text,
+	StyleSheet,
+	TextInput,
+	Image,
+	Button,
+	ScrollView,
+	TouchableWithoutFeedback,
+	Keyboard
+} from 'react-native'
+import { useDispatch } from 'react-redux'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import { AppHeaderIcon } from '../components/AppHeaderIcon'
+import { addPost } from '../redux/actions/postAction'
 
-export const CreateScreen = ({ }) => {
+export const CreateScreen = ({ navigation }) => {
+	const dispatch = useDispatch()
+	const [text, setText] = useState('')
+
+	const img = 'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg'
+
+	const saveHandler = () => {
+		const post = {
+			date: new Date().toJSON(),
+			text: text,
+			img: img,
+			booked: false
+		}
+		dispatch(addPost(post))
+		navigation.navigate('Main')
+	}
+
 	return (
-		<View style={styles.center}>
-			<Text>CreateScreen</Text>
-		</View>
+		<ScrollView>
+			<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+				<View style={styles.wrapper}>
+					<Text style={styles.title}>Создай новый пост</Text>
+					<TextInput
+						style={styles.textarea}
+						placeholder="Введите текст поста"
+						value={text}
+						onChangeText={setText}
+						multiline
+					/>
+					<Image style={{ width: '100%', height: 200, marginBottom: 10 }} source={{ uri: img }} />
+					<Button title="Создать пост" onPress={saveHandler} />
+				</View>
+			</TouchableWithoutFeedback>
+		</ScrollView>
+
 	)
 }
 
@@ -25,9 +67,17 @@ CreateScreen.navigationOptions = ({ navigation }) => ({
 })
 
 const styles = StyleSheet.create({
-	center: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
+	wrapper: {
+		padding: 10
+	},
+	title: {
+		fontSize: 20,
+		textAlign: 'center',
+		fontFamily: 'open-regular',
+		marginVertical: 10
+	},
+	textarea: {
+		padding: 10,
+		marginBottom: 10
 	}
 })
